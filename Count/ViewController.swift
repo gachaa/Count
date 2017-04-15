6129efaee53447bede88511fad30a69d7e7fa133
gachaa <mae.gachahero.104@gmail.com>
2017-04-15 14:59:31 +0900

    message

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,66 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        if(number >= 10){
+            label.textColor = UIColor.red
+        }else if(number <= -10){
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.white
+        }
+    }
+    
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+        if(number >= 10){
+            label.textColor = UIColor.red
+        }else if(number <= -10){
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.white
+        }
+    }
+    
+    @IBAction func kakeru(){
+        number = number * 2
+        label.text = String(number)
+        if(number >= 10){
+            label.textColor = UIColor.red
+        }else if(number <= -10){
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.white
+        }
+    }
+    
+    @IBAction func waru(){
+        number = number / 2
+        label.text = String(number)
+        if(number >= 10){
+            label.textColor = UIColor.red
+        }else if(number <= -10){
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.white
+        }
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
+        if(number >= 10){
+            label.textColor = UIColor.red
+        }else if(number <= -10){
+            label.textColor = UIColor.blue
+        }else{
+            label.textColor = UIColor.white
+        }
     }
 
 
